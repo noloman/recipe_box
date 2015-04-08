2f23089a95f41f7fed51109cf7cba58795e54d96
Manuel Lorenzo <mlorenzo@mobiquityinc.com>
2015-04-08 16:07:58 +0200

    generated model and routes and now REST methods created and working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,33 @@
 class RecipesController < ApplicationController
+    before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+    def index
+        @recipe = Recipe.all
+    end
+
+    def show
+    end
+
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+
+        if @recipe.save
+            redirect_to @recipe, notice: "Successfully created"
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def find_recipe
+        @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+        params.require(:recipe).permit(:title, :description)
+    end
 end
