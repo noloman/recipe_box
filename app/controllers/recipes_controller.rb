dc7d96a149729185bb721cad89799dc8f641228b
Manuel Lorenzo <mlorenzo@mobiquityinc.com>
2015-04-08 16:14:46 +0200

    added index sorted by creation date

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
     before_action :find_recipe, only: [:show, :edit, :update, :destroy]
     def index
-        @recipe = Recipe.all
+        @recipe = Recipe.all.order("created_at DESC")
     end
 
     def show
