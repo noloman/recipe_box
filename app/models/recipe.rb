57b5642647270cfc5ab165aa0ef17f57bc74c902
Manuel Lorenzo <mlorenzo@mobiquityinc.com>
2015-04-10 13:19:50 +0200

    added images with paperclip

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+    has_attached_file :image, :styles => { :medium => "400x400#" },
+    :default_url => "tarta-de-queso-y-brevas-2.jpg"
+    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408132534) do
+ActiveRecord::Schema.define(version: 20150410101849) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
